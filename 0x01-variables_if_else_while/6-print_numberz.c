9140b3b0ab3b7486a3267e40e88c8fd2653a06aa
0zyMandias007 <edmundcharwey11@gmail.com>
2022-06-04 03:36:57 +0000

    base 10 only charpu

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printing numbers no use of printf
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
