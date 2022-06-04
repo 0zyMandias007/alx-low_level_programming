a882975936fbd19c3ebef47c1eee103d9cd3e84e
0zyMandias007 <edmundcharwey11@gmail.com>
2022-06-04 04:33:38 +0000

    combination base 16

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
+	{
+		putchar((i %  10) + '0');
 		if (i == 9)
-			continue; 
-				putchar(',');
-				          putchar('');
-	putchar('\n');
+			continue;
+		putchar(',');
+		putchar('');
+	}
+	putchar('\n')
 
 	return (0);
 }
